d99dc7a6e90e48fad94da90f448ae587105c522e
Sudip Ghosh <ghoshsudip084@gmail.com>
2019-11-20 19:44:20 +0530

    Added control for the snake

--- app.js
+++ app.js
@@ -14,6 +14,28 @@ function setupCanvas(canvas) {
     c.scale(dpr, dpr);
     return c;
   }
+//Add control for the snake
+let d;
+document.addEventListener('keydown',direction);
+function direction(event)
+{
+  if(event.keyCode==37 && d!="RIGHT")
+  {
+    d="LEFT";
+  }
+  if(event.keyCode==38 && d!="DOWN")
+  {
+    d="UP";
+  }
+  if(event.keyCode==39 && d!="LEFT")
+  {
+    d="RIGHT";
+  }
+  if(event.keyCode==40 && d!="UP")
+  {
+    d="DOWN";
+  }
+}
 
 const canvas=document.getElementById('canvas');
 const c=setupCanvas(canvas);
